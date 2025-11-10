5fbff444b26815dc88057ede1ec5897b7396785f
Taskin Mohammad Mubassir <taskinmubassir@gmail.com>
2025-11-10 20:19:16 +0600

    work on isAuth, user.contriller, user.routes

---
+++ backend/controllers/user.controllers.js
@@ -0,0 +1,14 @@
+import User from '../models/user.model.js';
+
+export const getCurrentUser = async (req, res) => {
+  try {
+    let userId = req.userId;
+    let user = await User.findById(userId).select('-password');
+    if (!user) {
+      return res.status(400).json({ message: 'user not found' });
+    }
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: `current user error ${error}` });
+  }
+};
---
+++ backend/middlewares/isAuth.js
@@ -0,0 +1,23 @@
+import jsw from 'jsonwebtoken';
+
+const isAuth = async (req, res, next) => {
+  try {
+    let token = req.cookies.token;
+    if (!token) {
+      return res.status(400).json({ message: 'token is not found' });
+    }
+
+    let verifyToken = await JsonWebTokenError.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+    console.log(verifyToken);
+
+    req.userId = verifyToken.userId;
+    next();
+  } catch (error) {
+    return res.status(500).json({ message: `isauth error ${error}` });
+  }
+};
+
+export default isAuth;
